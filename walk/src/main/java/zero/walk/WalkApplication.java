19ac050a11e8e5edaf357d24fa507bec0e3c5799
kims26 <2tkdgus3@gmail.com>
2024-01-23 21:50:29 +0900

    test

---
+++ walk/src/main/java/zero/walk/WalkApplication.java
@@ -0,0 +1,13 @@
+package zero.walk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WalkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WalkApplication.class, args);
+	}
+
+}
---
+++ walk/src/test/java/zero/walk/WalkApplicationTests.java
@@ -0,0 +1,13 @@
+package zero.walk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WalkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
